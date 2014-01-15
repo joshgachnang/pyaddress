b11fa7df2ea8b6244f4207a7e1b2d34d54aad015
Darren Holland <hollanddd@gmail.com>
2014-01-15 10:46:53 -0600

    assertEqual provides more information at failure than assertTrue

--- address/test/test_address.py
+++ address/test/test_address.py
@@ -1,131 +1,110 @@
 import unittest
 from ..address import Address, AddressParser
 
-
 class AddressTest(unittest.TestCase):
     parser = None
 
     def setUp(self):
         self.parser = AddressParser()
-
+    
     def test_basic_full_address(self):
         addr = Address("2 N. Park Street, Madison, WI 53703", self.parser)
-        self.assertTrue(addr.house_number == "2")
-        self.assertTrue(addr.street_prefix == "N.")
-        self.assertTrue(addr.street == "Park")
-        self.assertTrue(addr.street_suffix == "St.")
-        self.assertTrue(addr.city == "Madison")
-        self.assertTrue(addr.state == "WI")
-        self.assertTrue(addr.zip == "53703")
-        self.assertTrue(addr.apartment == None)
-        # self.assertTrue(addr.building == None)
-
+        self.assertEqual(addr.house_number, "2")
+        self.assertEqual(addr.street_prefix, "N.")
+        self.assertEqual(addr.street, "Park")
+        self.assertEqual(addr.street_suffix, "St.")
+        self.assertEqual(addr.city, "Madison")
+        self.assertEqual(addr.state, "WI")
+        self.assertEqual(addr.zip, "53703")
+        self.assertEqual(addr.apartment, None)
+    
     def test_multi_address(self):
         addr = Address("416/418 N. Carroll St.", self.parser)
-        self.assertTrue(addr.house_number == "416")
-        self.assertTrue(addr.street_prefix == "N.")
-        self.assertTrue(addr.street == "Carroll")
-        self.assertTrue(addr.street_suffix == "St.")
-        self.assertTrue(addr.city == None)
-        self.assertTrue(addr.state == None)
-        self.assertTrue(addr.zip == None)
-        self.assertTrue(addr.apartment == None)
-        # self.assertTrue(addr.building == None)
-
+        self.assertEqual(addr.house_number, "416")
+        self.assertEqual(addr.street_prefix, "N.")
+        self.assertEqual(addr.street, "Carroll")
+        self.assertEqual(addr.street_suffix, "St.")
+        self.assertEqual(addr.city, None)
+        self.assertEqual(addr.state, None)
+        self.assertEqual(addr.zip, None)
+        self.assertEqual(addr.apartment, None)
+    
     def test_no_suffix(self):
         addr = Address("230 Lakelawn", self.parser)
-        self.assertTrue(addr.house_number == "230")
-        self.assertTrue(addr.street_prefix == None)
-        self.assertTrue(addr.street == "Lakelawn")
-        self.assertTrue(addr.street_suffix == None)
-        self.assertTrue(addr.city == None)
-        self.assertTrue(addr.state == None)
-        self.assertTrue(addr.zip == None)
-        self.assertTrue(addr.apartment == None)
-        # self.assertTrue(addr.building == None)
-
-#     def test_building_in_front(self):
-#         addr = Address("Roundhouse Apartments 626 Langdon", self.parser)
-# #        print addr
-#         self.assertTrue(addr.house_number == "626")
-#         self.assertTrue(addr.street_prefix == None)
-#         self.assertTrue(addr.street == "Langdon")
-#         self.assertTrue(addr.street_suffix == None)
-#         self.assertTrue(addr.city == None)
-#         self.assertTrue(addr.state == None)
-#         self.assertTrue(addr.zip == None)
-#         self.assertTrue(addr.apartment == None)
-#         # self.assertTrue(addr.building == "Roundhouse Apartments")
-
+        self.assertEqual(addr.house_number, "230")
+        self.assertEqual(addr.street_prefix, None)
+        self.assertEqual(addr.street, "Lakelawn")
+        self.assertEqual(addr.street_suffix, None)
+        self.assertEqual(addr.city, None)
+        self.assertEqual(addr.state, None)
+        self.assertEqual(addr.zip, None)
+        self.assertEqual(addr.apartment, None)
+    
     def test_streets_named_after_states(self):
         addr = Address("504 W. Washington Ave.", self.parser)
-        self.assertTrue(addr.house_number == "504")
-        self.assertTrue(addr.street_prefix == "W.")
-        self.assertTrue(addr.street == "Washington")
-        self.assertTrue(addr.street_suffix == "Ave.")
-        self.assertTrue(addr.city == None)
-        self.assertTrue(addr.state == None)
-        self.assertTrue(addr.zip == None)
-        self.assertTrue(addr.apartment == None)
-        # self.assertTrue(addr.building == None)
-
+        self.assertEqual(addr.house_number, "504")
+        self.assertEqual(addr.street_prefix, "W.")
+        self.assertEqual(addr.street, "Washington")
+        self.assertEqual(addr.street_suffix, "Ave.")
+        self.assertEqual(addr.city, None)
+        self.assertEqual(addr.state, None)
+        self.assertEqual(addr.zip, None)
+        self.assertEqual(addr.apartment, None)
+    
     def test_hash_apartment(self):
         addr = Address("407 West Doty St. #2", self.parser)
-        self.assertTrue(addr.house_number == "407")
-        self.assertTrue(addr.street_prefix == "W.")
-        self.assertTrue(addr.street == "Doty")
-        self.assertTrue(addr.street_suffix == "St.")
-        self.assertTrue(addr.city == None)
-        self.assertTrue(addr.state == None)
-        self.assertTrue(addr.zip == None)
-        self.assertTrue(addr.apartment == "#2")
-        # self.assertTrue(addr.building == None)
-
+        self.assertEqual(addr.house_number, "407")
+        self.assertEqual(addr.street_prefix, "W.")
+        self.assertEqual(addr.street, "Doty")
+        self.assertEqual(addr.street_suffix, "St.")
+        self.assertEqual(addr.city, None)
+        self.assertEqual(addr.state, None)
+        self.assertEqual(addr.zip, None)
+        self.assertEqual(addr.apartment, "#2")
+    
     def test_stray_dash_apartment(self):
         addr = Address("407 West Doty St. - #2", self.parser)
-        self.assertTrue(addr.house_number == "407")
-        self.assertTrue(addr.street_prefix == "W.")
-        self.assertTrue(addr.street == "Doty")
-        self.assertTrue(addr.street_suffix == "St.")
-        self.assertTrue(addr.city == None)
-        self.assertTrue(addr.state == None)
-        self.assertTrue(addr.zip == None)
-        self.assertTrue(addr.apartment == "#2")
-        # self.assertTrue(addr.building == None)
-
+        self.assertEqual(addr.house_number, "407")
+        self.assertEqual(addr.street_prefix, "W.")
+        self.assertEqual(addr.street, "Doty")
+        self.assertEqual(addr.street_suffix, "St.")
+        self.assertEqual(addr.city, None)
+        self.assertEqual(addr.state, None)
+        self.assertEqual(addr.zip, None)
+        self.assertEqual(addr.apartment, "#2")
+    
     def test_9_digit_zip(self):
         addr = Address("2 N. Park Street, Madison, WI 53703-0000", self.parser)
-        self.assertTrue(addr.house_number == "2")
-        self.assertTrue(addr.street_prefix == "N.")
-        self.assertTrue(addr.street == "Park")
-        self.assertTrue(addr.street_suffix == "St.")
-        self.assertTrue(addr.city == "Madison")
-        self.assertTrue(addr.state == "WI")
-        self.assertTrue(addr.zip == "53703-0000")
-        self.assertTrue(addr.apartment == None)
-
+        self.assertEqual(addr.house_number, "2")
+        self.assertEqual(addr.street_prefix, "N.")
+        self.assertEqual(addr.street, "Park")
+        self.assertEqual(addr.street_suffix, "St.")
+        self.assertEqual(addr.city, "Madison")
+        self.assertEqual(addr.state, "WI")
+        self.assertEqual(addr.zip, "53703-0000")
+        self.assertEqual(addr.apartment, None)
+    
     def test_9_digit_zip_no_dash(self):
         addr = Address("2 N. Park Street, Madison, WI 537030000", self.parser)
-        self.assertTrue(addr.house_number == "2")
-        self.assertTrue(addr.street_prefix == "N.")
-        self.assertTrue(addr.street == "Park")
-        self.assertTrue(addr.street_suffix == "St.")
-        self.assertTrue(addr.city == "Madison")
-        self.assertTrue(addr.state == "WI")
-        self.assertTrue(addr.zip == "537030000")
-        self.assertTrue(addr.apartment == None)
-
+        self.assertEqual(addr.house_number, "2")
+        self.assertEqual(addr.street_prefix, "N.")
+        self.assertEqual(addr.street, "Park")
+        self.assertEqual(addr.street_suffix, "St.")
+        self.assertEqual(addr.city, "Madison")
+        self.assertEqual(addr.state, "WI")
+        self.assertEqual(addr.zip, "537030000")
+        self.assertEqual(addr.apartment, None)
+    
     def test_suffixless_street_with_city(self):
         addr = Address("431 West Johnson, Madison, WI", self.parser)
-        self.assertTrue(addr.house_number == "431")
-        self.assertTrue(addr.street_prefix == "W.")
-        self.assertTrue(addr.street == "Johnson")
-        self.assertTrue(addr.street_suffix == None)
-        self.assertTrue(addr.city == "Madison")
-        self.assertTrue(addr.state == "WI")
-        self.assertTrue(addr.zip == None)
-        self.assertTrue(addr.apartment == None)
-        # self.assertTrue(addr.building == None)
+        self.assertEqual(addr.house_number, "431")
+        self.assertEqual(addr.street_prefix, "W.")
+        self.assertEqual(addr.street, "Johnson")
+        self.assertEqual(addr.street_suffix, None)
+        self.assertEqual(addr.city, "Madison")
+        self.assertEqual(addr.state, "WI")
+        self.assertEqual(addr.zip, None)
+        self.assertEqual(addr.apartment, None)
     
     def test_multi_word_city(self):
         addr = Address('351 King St. #400, San Francisco, CA, 94158', self.parser)
@@ -138,19 +117,6 @@ class AddressTest(unittest.TestCase):
         self.assertEqual('NE', addr.post_direction)
     
 
-    # Not yet passing.
-    #def test_5_digit_house_number(self):
-    #    addr = Address('51691 North Scottsdale Road', self.parser)
-    #    self.assertTrue(addr.house_number == "51691")
-    #    self.assertTrue(addr.street_prefix == "N.")
-    #    self.assertTrue(addr.street == "Scottsdale")
-    #    self.assertTrue(addr.street_suffix == "Rd.")
-    #    self.assertTrue(addr.city == None)
-    #    self.assertTrue(addr.state == None)
-    #    self.assertTrue(addr.zip == None)
-    #    self.assertTrue(addr.apartment == None)
-
-
 class AddressParserTest(unittest.TestCase):
     ap = None
 
@@ -158,17 +124,13 @@ class AddressParserTest(unittest.TestCase):
         self.ap = AddressParser()
 
     def test_load_suffixes(self):
-        self.assertTrue(self.ap.suffixes["ALLEY"] == "ALY")
+        self.assertEqual(self.ap.suffixes["ALLEY"], "ALY")
 
     def test_load_cities(self):
         self.assertTrue("wisconsin rapids" in self.ap.cities)
 
     def test_load_states(self):
-        self.assertTrue(self.ap.states["Wisconsin"] == "WI")
-
-    # Not using preloaded streets any more.
-#    def test_load_streets(self):
-#        self.assertTrue("mifflin" in self.ap.streets)
-
+        self.assertEqual(self.ap.states["Wisconsin"], "WI")
+        
 if __name__ == '__main__':
     unittest.main()
